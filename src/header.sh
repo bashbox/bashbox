cb8b653ea4460850d0ffc059a989f736ea7f6967
AXON <axonasif@gmail.com>
2021-06-28 21:39:17 +0600

    Update README.md

--- src/header.sh
+++ src/header.sh
@@ -1,10 +1,10 @@
 function bb_bootstrap_header() {
 
-	set -o pipefail; # To grab the last return code from a pipe.
-	set -o errexit; # To exit immadiately after trapping ERR.
-	set -o errtrace; # To detect ERR on some bash builtin commands.
-	set -o nounset; # To avoid unexpected missing variables.
-	shopt -s expand_aliases; # To enable alias bash-builtin usage without interactive mode.
+set -o pipefail; # To grab the last return code from a pipe.
+set -o errexit; # To exit immadiately after trapping ERR.
+set -o errtrace; # To detect ERR on some bash builtin commands.
+set -o nounset; # To avoid unexpected missing variables.
+shopt -s expand_aliases; # To enable alias bash-builtin usage without interactive mode.
 	alias use='BB_USE_ARGS=("$@"); BB_SOURCE="${BASH_SOURCE[0]}" __use_func'; 
 	trap 'BB_ERR_SOURCE="${BASH_SOURCE[0]}" println::error "$BASH_COMMAND" $?' ERR; 
 	_main_src_dir="$(dirname "$(readlink -f "$0")")"; # TODO: Needs review
