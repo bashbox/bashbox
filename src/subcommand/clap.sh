585c45edcd25da37beb88c58c5bb2642ed19a5bf
AXON <axonasif@gmail.com>
2021-07-11 18:46:28 +0600

    fix(subcommand::clap): Unnecessary begins_with_short_option()

--- src/subcommand/clap.sh
+++ src/subcommand/clap.sh
@@ -11,13 +11,6 @@
 # Argbash is FREE SOFTWARE, see https://argbash.io for more info
 
 
-begins_with_short_option()
-{
-	local first_option all_short_options='h';
-	first_option="${1:0:1}";
-	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0;
-}
-
 # THE DEFAULTS INITIALIZATION - POSITIONALS
 _positionals=();
 _arg_path=;
