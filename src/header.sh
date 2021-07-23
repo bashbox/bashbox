bbf5acf8dc104c1e9f29622826a9fbdea731fd75
AXON <axonasif@gmail.com>
2021-07-23 18:48:53 +0600

    fix(bootstrap-header): Unset external aliases

--- src/header.sh
+++ src/header.sh
@@ -3,6 +3,7 @@ function bb_bootstrap_header() {
 	set -o errexit; # To exit immadiately after trapping ERR.
 	set -o errtrace; # To detect ERR on some bash builtin commands.
 	set -o nounset; # To avoid unexpected missing variables.
+	\command \unalias -a; # To Make sure external aliases are not interfering.
 	shopt -s expand_aliases; # To enable alias bash-builtin usage without interactive mode.
 	trap 'BB_ERR_SOURCE="${BASH_SOURCE[0]}" println::error "$BASH_COMMAND" $?' ERR;
 }
