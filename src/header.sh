fe6ecbe895d49801aed8d338eade02552cd0eff1
AXON <axonasif@gmail.com>
2021-07-24 21:10:42 +0600

    chore: TODO comment

--- src/header.sh
+++ src/header.sh
@@ -5,5 +5,6 @@ function bb_bootstrap_header() {
 	set -o nounset; # To avoid unexpected missing variables.
 	\command \unalias -a; # To Make sure external aliases are not interfering.
 	shopt -s expand_aliases; # To enable alias bash-builtin usage without interactive mode.
+	# TODO: Use `caller` builtin for stacktrace instead. (Check my saved meme notes)
 	trap 'BB_ERR_SOURCE="${BASH_SOURCE[0]}" println::error "$BASH_COMMAND" $?' ERR;
 }
