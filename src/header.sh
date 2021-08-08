beafeee0d50e868b5c4be3cbba77cb9df90fa5b1
AXON <axonasif@gmail.com>
2021-08-08 12:49:27 +0600

    feat(bootstrap-header): Implement STACK TRACE

--- src/header.sh
+++ src/header.sh
@@ -1,15 +1,42 @@
 function bb_bootstrap_header() {
 	function log::error() {
-		local _retcode="${2:-$?}";
+		local _lasttoken="$_";
+		local _exception_line="${1}";
+		local _retcode="${2}";
+		local IFS=$' \t\n'
 		local _source="${BB_ERR_SOURCE:-"${BASH_SOURCE[-1]}"}";
 
-		>&2 echo -e "[!!!] \033[1;31merror\033[0m[$_retcode]: ${_source}[$BASH_LINENO]: $1";
+		>&2 echo -e "[!!!] \033[1;31merror\033[0m[$_retcode]: ${_source##*/}[$BASH_LINENO]: ${STACK_TRACE:-"$_exception_line"}";
+
+		# Handle stacktrace
+		if test -v STACK_TRACE; then {
+			echo -e "STACK TRACE: (TOKEN: $_lasttoken) $_exception_line";
+			# local -i startFrom="1";
+			local -i _frame=0;
+			# local _stack;
+			local _treestack='|--';
+			local _line _caller _source;
+
+			# while _stack="$(caller $_frame 2>&1)"; do { 
+			while read -r _line _caller _source < <(caller "$_frame"); do {
+				# if (( $_frame + 1 >= $startFrom ))
+				# then
+					# local -a _trace=( $_stack );
+					# echo "${_treestack} ${_trace[1]} @@ ${_trace[@]:2}::${_trace[0]}";
+				# fi
+				echo "$_treestack ${_caller} >> ${_source##*/}::${_line}";
+				_frame+=1;
+				_treestack+='--';
+
+			} done
+
+		} fi
+
 		return "$_retcode";
 	}
-	# TODO: Use `caller` builtin for stacktrace instead. (Check my saved meme notes)
 	\command \trap 'exit' USR1; # A workaround to properly catch error status from process substitution.
 								# Eg `while read -r _bruh; do echo bruh; done < <(false)`.
-	\command \trap 'log::error "$BASH_COMMAND" || { kill -USR1 "$___self_PID"; }' ERR;
+	\command \trap 'STACK_TRACE="UNCAUGHT EXCEPTION" log::error "$BASH_COMMAND" $? || { kill -USR1 "$___self_PID"; }' ERR;
 
 	\command \unalias -a; # To Make sure external aliases are not interfering.
 	# set -o pipefail; # To grab the last return code from a pipe.
